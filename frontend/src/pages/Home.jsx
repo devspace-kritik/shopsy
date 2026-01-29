63276cc4b2750d0ac274d7e4b33e6b9ad3c44ff3
devspace_kritik <215986474+devspace-kritik@users.noreply.github.com>
2026-01-29 23:01:12 +0530

    Created Home page- Created Latest collection, Best seller, Policy, Newsletter and Footer components with styles

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,19 @@
+import Bestseller from '@/components/Bestseller'
+import Hero from '@/components/Hero'
+import LatestCollection from '@/components/LatestCollection'
+import NewsletterBox from '@/components/NewsletterBox'
+import OurPolicy from '@/components/OurPolicy'
 import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <Hero/>
+      <LatestCollection/>
+      <Bestseller/>
+      <OurPolicy/>
+      <NewsletterBox/>
+    </div>
   )
 }
 
