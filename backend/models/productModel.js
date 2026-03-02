2ffb905aaea87e7813184352042b5644822d942a
devspace_kritik <215986474+devspace-kritik@users.noreply.github.com>
2026-03-02 22:52:11 +0530

    Created controllers & routes for login, register & admin  in userController & userRoute files

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -13,6 +13,6 @@ const productSchema = new mongoose.Schema({
 
 })
 
-const productModel =mongoose.models.product || mongoose.model("product", productSchema);
+const productModel = mongoose.models.product || mongoose.model("product", productSchema);
 
 export default productModel
