bef09c16b30c2f219adde49099ff26b6a0fa9160
devspace_kritik <215986474+devspace-kritik@users.noreply.github.com>
2026-03-03 23:00:08 +0530

    Worked on user controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,17 +1,17 @@
 
 // Route for user login
 const loginUser = async (req, res) => {
-    res.json({msg: "Register API working"})
+    res.json({msg: "Login API working"})
 }
 
 // Route for user register
 const registerUser = async (req, res) => {
-
+    res.json({msg: "Register API working"})
 }
 
 // Route for admin login
 const adminLogin = async (req, res) => {
-
+    res.json({msg: "Admin Login API working"})
 }
 
 export {loginUser, registerUser, adminLogin}
