1b342f0d878770be05b40ea2685bef723b16c7cd
devspace_kritik <215986474+devspace-kritik@users.noreply.github.com>
2026-03-05 23:20:37 +0530

    Added validation functionality for registerUser

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,3 +1,4 @@
+import vlidator from "validator";
 
 // Route for user login
 const loginUser = async (req, res) => {
@@ -6,7 +7,26 @@ const loginUser = async (req, res) => {
 
 // Route for user register
 const registerUser = async (req, res) => {
-    res.json({msg: "Register API working"})
+    try {
+        const {name, email, password} = req.body;
+
+        //Checking user already exists or not
+        const exists = await userModel.findOne({email})
+        if(exists){
+            return res.json({success: false, message: "User already exists"});
+        }
+
+        //Validating email format and strong password
+        if(!validator.isEmail){
+            return res.json({success: false, message: "Please enter a valid email"});
+        }
+        if(password.length < 8){
+            return res.json({success: false, message: "Please enter a sttrong password"});
+        }
+        
+    } catch (error) {
+        
+    }
 }
 
 // Route for admin login
