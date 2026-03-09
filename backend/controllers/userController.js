2c5dad8bc4526c76e84c50be7245ee66ddb90b96
devspace_kritik <215986474+devspace-kritik@users.noreply.github.com>
2026-03-09 21:39:52 +0530

    Worked on user model and controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -7,26 +7,26 @@ const loginUser = async (req, res) => {
 
 // Route for user register
 const registerUser = async (req, res) => {
-    try {
-        const {name, email, password} = req.body;
+    // try {
+    //     const {name, email, password} = req.body;
 
-        //Checking user already exists or not
-        const exists = await userModel.findOne({email})
-        if(exists){
-            return res.json({success: false, message: "User already exists"});
-        }
+    //     //Checking user already exists or not
+    //     const exists = await userModel.findOne({email})
+    //     if(exists){
+    //         return res.json({success: false, message: "User already exists"});
+    //     }
 
-        //Validating email format and strong password
-        if(!validator.isEmail){
-            return res.json({success: false, message: "Please enter a valid email"});
-        }
-        if(password.length < 8){
-            return res.json({success: false, message: "Please enter a sttrong password"});
-        }
+    //     //Validating email format and strong password
+    //     if(!validator.isEmail){
+    //         return res.json({success: false, message: "Please enter a valid email"});
+    //     }
+    //     if(password.length < 8){
+    //         return res.json({success: false, message: "Please enter a sttrong password"});
+    //     }
         
-    } catch (error) {
+    // } catch (error) {
         
-    }
+    // }
 }
 
 // Route for admin login
