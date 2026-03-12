3eb8b16de91325b45061651af75329d17ef97e6d
devspace_kritik <215986474+devspace-kritik@users.noreply.github.com>
2026-03-12 23:09:56 +0530

    Worked on product and user schema

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -5,11 +5,11 @@ const productSchema = new mongoose.Schema({
     description: {type:String, required:true},
     price: {type: Number, required:true},
     image: {type: Array, required:true},
-    // category: {type: Number, required:true},
-    // subCategory: {type: Number, required:true},
-    // sizes: {type: Array, required:true},
-    // bestseller: {type: Boolean},
-    // date: {type: Number, required:true},
+    category: {type: Number, required:true},
+    subCategory: {type: Number, required:true},
+    sizes: {type: Array, required:true},
+    bestseller: {type: Boolean},
+    date: {type: Number, required:true},
 
 })
 
