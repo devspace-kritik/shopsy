fc22c4887943f5ac498d6a7e998f9014b8fc6ddf
devspace_kritik <215986474+devspace-kritik@users.noreply.github.com>
2026-03-15 00:01:05 +0530

    Added cloudinary setup in the project

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -14,7 +14,7 @@ import mongoose from 'mongoose';
 const connectDB = async () =>{
     mongoose.connection.on('connected', ()=>{
         console.log("DB Connected");
-    })
+    });
     
     await mongoose.connect(`${process.env.MONGODB_URI}/e-commerce`);
 }
