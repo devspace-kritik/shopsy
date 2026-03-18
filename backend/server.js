52dbcdf666a46a58070397f26ead85b0168db72a
devspace_kritik <215986474+devspace-kritik@users.noreply.github.com>
2026-03-18 22:06:57 +0530

    Added product controller, product route files

--- backend/server.js
+++ backend/server.js
@@ -30,6 +30,7 @@ import cors from 'cors';
 import 'dotenv/config';
 import connectDB from './config/mongodb.js';
 import connectCloudinary from './config/cloudinary.js';
+import productRouter from './routes/productRoute.js';
 
 // App config
 const app = express();
@@ -42,6 +43,9 @@ app.use(express.json());
 app.use(cors());
 
 // Api endpoints
+app.use('./api/user',userRouter);
+app.use('./api.product', productRouter);
+
 app.get('/', (req, res)=>{
     res.send("API Working");
 });
